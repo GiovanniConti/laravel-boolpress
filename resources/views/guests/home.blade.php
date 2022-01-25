3539e53fcdcdfcd44c53849ceb585dae12d9287e
GiovanniConti <giovanni.conti@live.it>
2022-01-25 09:54:59 +0100

    updated bootstrap from v4 to v5 and removed jquery

--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -8,8 +8,6 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-        {{-- CSS Bootstrap --}}
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
         <!-- Styles -->
         <style>
